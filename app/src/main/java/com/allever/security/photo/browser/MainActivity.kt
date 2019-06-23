4c20eec18f1aaaec9a109f2b3ca04739b412c38e
devallever <devallever@163.com>
2019-06-23 16:46:59 +0800

    init.

---
+++ app/src/main/java/com/allever/security/photo/browser/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.allever.security.photo.browser
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
