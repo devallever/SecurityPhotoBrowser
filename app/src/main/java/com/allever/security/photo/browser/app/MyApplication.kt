bdac31f245fccdb00f68cf6e1ad25b89412cd132
devallever <devallever@163.com>
2019-06-23 22:13:58 +0800

    内容： 提交代码

---
+++ app/src/main/java/com/allever/security/photo/browser/app/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.allever.security.photo.browser.app
+
+import com.allever.lib.common.app.App
+
+class MyApplication: App() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
