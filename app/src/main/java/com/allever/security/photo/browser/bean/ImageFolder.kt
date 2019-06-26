7b9f6c8e94a8435e41f415fc9bc54c39a2fccff9
devallever <devallever@163.com>
2019-06-26 16:27:29 +0800

    内容： 添加旧代码

---
+++ app/src/main/java/com/allever/security/photo/browser/bean/ImageFolder.kt
@@ -0,0 +1,59 @@
+package com.allever.security.photo.browser.bean
+
+import java.util.ArrayList
+
+class ImageFolder {
+    /**
+     * 图片的文件夹路径(包含名称)
+     */
+    var dir: String? = null
+
+    /**
+     * 文件夹的名称
+     */
+    var name: String? = null
+
+    /**
+     * 文件夹的名称
+     */
+    var bucketId: String? = null
+
+    /**
+     * 图片的ThumbnailBean
+     */
+    var data: ArrayList<ThumbnailBean>? = null
+
+    /**
+     * 图片的数量
+     */
+    var count: Int = 0
+
+    /**
+     * 第一张图片的ThumbnailBean
+     */
+    var firstThumbnailBean: ThumbnailBean? = null
+        private set
+
+    var isNeedRefresh: Boolean = false
+
+    val isDataInit: Boolean
+        get() = data != null
+
+    constructor() {
+        count = 0
+    }
+
+    constructor(count: Int) {
+        this.count = count
+    }
+
+    fun setDirAndName(dir: String) {
+        this.dir = dir
+        val lastIndexOf = this.dir!!.lastIndexOf("/")
+        this.name = this.dir!!.substring(lastIndexOf)
+    }
+
+    fun setFirstImageBean(thumbnailBean: ThumbnailBean) {
+        this.firstThumbnailBean = thumbnailBean
+    }
+}
