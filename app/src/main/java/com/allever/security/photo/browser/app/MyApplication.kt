0199027751f35cfb112a1a8900584d66b54171fb
devallever <devallever@163.com>
2019-08-25 11:04:06 +0800

    内容：集成友盟统计

--- app/src/main/java/com/allever/security/photo/browser/app/MyApplication.kt
+++ app/src/main/java/com/allever/security/photo/browser/app/MyApplication.kt
@@ -1,10 +1,12 @@
 package com.allever.security.photo.browser.app
 
 import com.allever.lib.common.app.App
+import com.allever.lib.umeng.UMeng
 
 class MyApplication : App() {
     override fun onCreate() {
         super.onCreate()
         com.android.absbase.App.setContext(this)
+        UMeng.init(this, "5d61ebd3570df3add50003a6", "google")
     }
 }
