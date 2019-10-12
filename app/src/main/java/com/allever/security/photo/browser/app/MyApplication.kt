739d049d9e4cb100abfdeb38da46331a17b93f12
devallever <devallever@163.com>
2019-10-12 11:12:12 +0800

    内容：整理代码

--- app/src/main/java/com/allever/security/photo/browser/app/MyApplication.kt
+++ app/src/main/java/com/allever/security/photo/browser/app/MyApplication.kt
@@ -1,7 +1,12 @@
 package com.allever.security.photo.browser.app
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.text.TextUtils
 import com.allever.lib.common.app.App
+import com.allever.lib.common.util.DLog
 import com.allever.lib.umeng.UMeng
+import com.allever.security.photo.browser.BuildConfig
 
 class MyApplication : App() {
     override fun onCreate() {
